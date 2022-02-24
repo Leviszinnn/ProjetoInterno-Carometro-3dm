fcf021dc9b346873fafe08d1c00ad693852d8c89
Gustavo Borges de Souza <gustavo.souza14@portalsesisp.org.br>
2022-02-24 09:43:53 -0300

    estrutur rotas feitas

--- Frontend/carometro-ui/src/index.js
+++ Frontend/carometro-ui/src/index.js
@@ -4,10 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import { parseJWT, usuarioAutenticado } from './services/auth';
+
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        {/*
+        Troca pelo que a gente fizer
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/notFound" component={NotFound} /> 
+        <Redirect to="/notFound" /> */}
+      </Switch>
+    </div>
+  </Router>
+)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  routing,
   document.getElementById('root')
 );
 
