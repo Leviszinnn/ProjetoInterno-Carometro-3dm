f3fe93d5df2fedf88e0b0f0a4a1776de4e324699
Levi Bueno Da Silva <43717830821@info.scs>
2022-02-25 07:31:01 -0300

    cadastro feito

---
+++ Backend/ProjetoInternoCarometro/ProjetoInternoCarometro/Interfaces/IAlunoRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProjetoInternoCarometro.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoInternoCarometro.Interfaces
+{
+    interface IAlunoRepository
+    {
+        void cadastrarAluno(Aluno aluno);
+    }
+}
