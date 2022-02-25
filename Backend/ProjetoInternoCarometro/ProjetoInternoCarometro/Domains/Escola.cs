19334884b2067e25203ea2a6a70611bc9bb81947
Levi Bueno Da Silva <43717830821@info.scs>
2022-02-25 06:59:23 -0300

    scaffold realizado

---
+++ Backend/ProjetoInternoCarometro/ProjetoInternoCarometro/Domains/Escola.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ProjetoInternoCarometro.Domains
+{
+    public partial class Escola
+    {
+        public Escola()
+        {
+            Alunos = new HashSet<Aluno>();
+            Professors = new HashSet<Professor>();
+        }
+
+        public int IdEscola { get; set; }
+        public string NomeEscola { get; set; }
+        public string Endereco { get; set; }
+        public int Numero { get; set; }
+
+        public virtual ICollection<Aluno> Alunos { get; set; }
+        public virtual ICollection<Professor> Professors { get; set; }
+    }
+}
